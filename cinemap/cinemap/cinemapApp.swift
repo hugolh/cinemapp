f27b93c809be087f812686f8ee65737540712aec
hugolh <75566783+hugolh@users.noreply.github.com>
2024-06-04 21:19:23 +0200

    init

---
+++ cinemap/cinemap/cinemapApp.swift
@@ -0,0 +1,17 @@
+//
+//  cinemapApp.swift
+//  cinemap
+//
+//  Created by Le Hen Hugo on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct cinemapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
